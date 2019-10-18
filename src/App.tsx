1849143e6bb10676a8e70f96da3f49ef827e4b0f
Charles Cunningham <c.a.cunningham6@gmail.com>
2019-10-18 19:19:51 +0200

    most basic version

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import React from 'react';
 import './App.css';
+import { D3Component } from './graph';
 
 const App: React.FC = () => {
   return (
     <div className="App">
+        <D3Component data={[1, 2, 3]}/>
     </div>
   );
 }
