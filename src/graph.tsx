ae552ef4c1896ab8a4233a0ef2ec1d44912b512e
Charles Cunningham <c.a.cunningham6@gmail.com>
2019-10-23 19:43:44 +0200

    replace default with HOCs

--- src/graph.tsx
+++ src/graph.tsx
@@ -36,27 +36,17 @@ export const D3Component = (props: IProps) => {
                 const svg = d3.select(d3Container.current);
 
                 // Bind D3 data
-                const update = svg
-                    .append('g')
-                    .selectAll('text')
-                    .data(props.data);
-
-                // Enter new D3 elements
-                update.enter()
-                    .append('text')
-                    .attr('x', (d, i) => i * 25)
-                    .attr('y', 40)
-                    .style('font-size', 24)
-                    .text((d: number) => d);
+                const links = getLinks(svg)(demo.links)
+                const nodes = getNodes(svg)(demo.nodes)
 
                 // Update existing D3 elements
-                update
-                    .attr('x', (d, i) => i * 40)
-                    .text((d: number) => d);
+                // update
+                    // .attr('x', (d, i) => i * 40)
+                    // .text((d: number) => d);
 
                 // Remove old D3 elements
-                update.exit()
-                    .remove();
+                // update.exit()
+                    // .remove();
             }
         },
 
