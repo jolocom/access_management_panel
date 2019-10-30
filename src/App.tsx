d587b05eccee8c362690bd22db084560b425b46a
Charles Cunningham <c.a.cunningham6@gmail.com>
2019-10-30 20:20:28 +0100

    new impl, does sim before rendering

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,12 @@
 import React from 'react';
 import './App.css';
-import { D3Component } from './graph';
+import { GraphComponent } from './g2';
 import { graph } from './demoGraph'
-import { getD3Graph } './graphLib'
-
-const d3Graph = getD3Graph(graph)
 
 const App: React.FC = () => {
   return (
     <div className="App">
-          <D3Component data={d3Graph}/>
+          <GraphComponent graph={graph}/>
     </div>
   );
 }
--- src/graphLib.ts
+++ src/graphLib.ts
@@ -28,30 +28,3 @@ export type FileGraph = {
     doors: Door[]
 }
 
-// D3 node
-export type d3Node = {
-    group: number
-}
-
-export type d3Link = Link & {
-    value: number
-}
-
-export const getD3Graph = (file: FileGraph): Graph => ({
-    nodes: file.rooms.map(room => ({
-        ...room,
-        group: 1
-    })),
-    links: file.doors.map(door => ({
-        ...door,
-        value: 10
-    }))
-})
-
-export type d3Door = d3Link & Door
-export type d3Room = d3Node & Room
-
-export type Graph = {
-    nodes: d3Room[],
-    links: d3Door[]
-}
