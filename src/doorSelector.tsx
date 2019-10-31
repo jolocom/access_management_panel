bd8ac35b20234f6a34e247b628c7afde956e64c9
Charles Cunningham <c.a.cunningham6@gmail.com>
2019-10-31 15:35:26 +0100

    add selection state to doorSelector

--- src/doorSelector.tsx
+++ src/doorSelector.tsx
@@ -1,18 +1,24 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Button } from 'react-bootstrap';
 import { GraphComponent } from './graph';
 import { FileGraph, Door } from './graphLib';
 
 interface IProps {
     graph: FileGraph
-    onSelectionFinished: (selection: Door[]) => void
+    onSelectionFinished: (selection: string[]) => void
 }
 
 export const DoorSelector: React.FC<IProps>= (props: IProps) => {
+    const [selection, setSelection] = useState<string[]>([])
+
     return (
         <div>
-            <GraphComponent graph={props.graph} onLinkClicked={console.log}/>
-            <Button onClick={() => console.log("h")}>henlo</Button>
+            <GraphComponent graph={props.graph} onLinkClicked={({id}) => setSelection(
+                selection.some(s => id === s)
+                    ? selection.filter(s => s !== id)
+                    : [...selection, id]
+            )}/>
+            <Button onClick={() => props.onSelectionFinished(selection)}>henlo</Button>
         </div>
     )
 }
