2ea63bd25dcffbc187cd5e6e9c18551935165e59
Charles Cunningham <c.a.cunningham6@gmail.com>
2019-11-01 12:00:05 +0100

    replace button and simplify selector

--- src/doorSelector.tsx
+++ src/doorSelector.tsx
@@ -1,11 +1,15 @@
 import React, { useState } from 'react';
-import { Button } from 'react-bootstrap';
+import { Button } from '@acpaas-ui/react-components';
 import { GraphRenderer } from './graph';
 import { FileGraph } from './graphLib';
 
 interface IProps {
     graph: FileGraph
     onSelectionFinished: (selection: string[]) => void
+    style: {
+        width: number,
+        height: number
+    }
 }
 
 interface IState {
@@ -13,26 +17,21 @@ interface IState {
 }
 
 export class DoorSelector extends React.Component<IProps, IState> {
-    onFinish: (selection: string[]) => void;
-    readonly graph: FileGraph
-    
     constructor(props: IProps) {
         super(props);
-        this.graph = props.graph
-        this.onFinish = props.onSelectionFinished
         this.state = { selection: [] }
     }
 
     render() {
         return (
-        <div>
-                <GraphRenderer graph={this.graph} style={{width: 400, height: 200}} onLinkClicked={id => {
+            <div>
+                <GraphRenderer graph={this.props.graph} style={this.props.style} onLinkClicked={id => {
                     if (this.state.selection.includes(id)) this.setState({ selection: this.state.selection.filter(s => s !== id) })
                     else this.setState({ selection: [...this.state.selection, id] })
-                }}/>
-                <Button onClick={() => this.onFinish(this.state.selection)}>issue</Button>
+                }} />
+                <Button onClick={() => this.props.onSelectionFinished(this.state.selection)}>issue</Button>
                 <p>{this.state.selection}</p>
-        </div>
+            </div>
         )
     }
 }
