147be57118bb611f86fcfdfdeec365ffe326c5c2
Charles Cunningham <c.a.cunningham6@gmail.com>
2019-11-04 14:30:43 +0100

    final fixes

--- src/index.tsx
+++ src/index.tsx
@@ -1,4 +1,5 @@
-import React, { useRef, useEffect } from 'react';
+import React from 'react';
+const { useRef, useEffect } = React
 import * as d3 from 'd3'
 import { Graph, Node, Link } from './graphLib';
 
